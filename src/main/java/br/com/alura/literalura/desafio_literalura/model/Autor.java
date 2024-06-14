8290f3527a5c659436a48c666346309ff434f104
Bruno <bfaria81@hotmail.com>
2024-06-14 17:26:06 -0300

    atualizacaoo projeto

---
+++ src/main/java/br/com/alura/literalura/desafio_literalura/model/Autor.java
@@ -0,0 +1,45 @@
+package br.com.alura.literalura.desafio_literalura.model;
+
+public class Autor {
+
+    private Long id;
+    private String autor;
+    private Integer nascimento;
+    private Integer falecimento;
+
+    public Autor(String autor) {
+        this.autor = autor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Integer nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public Integer getFalecimento() {
+        return falecimento;
+    }
+
+    public void setFalecimento(Integer falecimento) {
+        this.falecimento = falecimento;
+    }
+}
